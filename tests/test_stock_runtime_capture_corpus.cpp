134a1f1853e28c031144dca545afa68f1d98dd82
Vitaly <pvitaly91@gmail.com>
2026-09-24 19:16:54 +0300

    Include range algorithm declarations explicitly in tests

--- tests/test_stock_runtime_capture_corpus.cpp
+++ tests/test_stock_runtime_capture_corpus.cpp
@@ -3,6 +3,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 
+#include <algorithm>
 #include <array>
 #include <atomic>
 #include <chrono>
--- tests/test_stock_spawn_request.cpp
+++ tests/test_stock_spawn_request.cpp
@@ -2,6 +2,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 
+#include <algorithm>
 #include <array>
 #include <cstddef>
 #include <cstdint>
